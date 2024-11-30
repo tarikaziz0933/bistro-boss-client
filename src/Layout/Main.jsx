a8ba75428e4750feeebcad44d9f152d959b02449
tarikulaziz <tarikaziz0933@gmail.com>
2024-11-30 10:29:24 +0600

    created homme page

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../SharedPage/Footer/Footer";
+import NavBar from "../SharedPage/NavBar/NavBar";
+
+const Main = () => {
+    return (
+        <div>
+            <NavBar></NavBar>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
---
+++ src/Pages/Home/Category/Category.jsx
@@ -0,0 +1,45 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { FreeMode, Pagination } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/pagination';
+
+import slide1 from "../../../assets/assets/home/slide1.jpg";
+import slide2 from "../../../assets/assets/home/slide2.jpg";
+import slide3 from "../../../assets/assets/home/slide3.jpg";
+import slide4 from "../../../assets/assets/home/slide4.jpg";
+import slide5 from "../../../assets/assets/home/slide5.jpg";
+
+const Category = () => {
+    return (
+        <Swiper
+            slidesPerView={3}
+            spaceBetween={30}
+            freeMode={true}
+            pagination={{
+                clickable: true,
+            }}
+            modules={[FreeMode, Pagination]}
+            className="mySwiper"
+        >
+            <SwiperSlide>
+                <img src={slide1} alt="" />
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src={slide2} alt="" />
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src={slide3} alt="" />
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src={slide4} alt="" />
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src={slide5} alt="" />
+            </SwiperSlide>
+        </Swiper>
+    );
+};
+
+export default Category;
